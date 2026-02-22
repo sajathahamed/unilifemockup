f71ec58f629bd6b4739b0277208699f7401b5145
sajjath <easytech6727@gmail.com>
2026-02-22 11:40:46 +0530

    remove mobile app and create seperate repo

--- mobile/src/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export { supabase, signInWithEmail, signUpWithEmail, signOut, resetPassword, getCurrentUser, getSession, getUserRole, getUserProfile } from './supabase';
-export { signInWithGoogle, onAuthStateChange } from './google-auth';
